cab18c75a8d46041726c6d1bdd40f9cb3591041c
Iamatto01 <145031320+Iamatto01@users.noreply.github.com>
2024-05-11 19:46:08 +0800

    Update MyWorld.java

--- PacMan/MyWorld.java
+++ PacMan/MyWorld.java
@@ -1,63 +1,26 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-public class Player extends Actor
+
+ // @author (Muhammad Saifudin) 
+
+public class MyWorld extends World
 {
-    private int speed = 3;  // Set the speed of the player
 
-    // Load the images for each direction
-    private GreenfootImage imageUp;
-    private GreenfootImage imageDown;
-    private GreenfootImage imageLeft;
-    private GreenfootImage imageRight;
 
-    public Player()
-    {
-        imageUp = new GreenfootImage("image1x4.png");
-        imageDown = new GreenfootImage("image1x1.png");
-        imageLeft = new GreenfootImage("image1x2.png");
-        imageRight = new GreenfootImage("image1x3.png");
-
-        scaleImage(imageUp);
-        scaleImage(imageDown);
-        scaleImage(imageLeft);
-        scaleImage(imageRight);
-
-        setImage(imageUp);
+    public MyWorld()
+    {    
+    
+        super(600, 400, 1); 
+        GreenfootImage background = new GreenfootImage(getWidth(), getHeight());
+        background.setColor(Color.GREEN);
+        background.fill();
+        setBackground(background);
+        prepare();
     }
-
-    private void scaleImage(GreenfootImage image)
+ 
+    private void prepare()
     {
-        int newWidth = image.getWidth() * 50 / 100;
-        int newHeight = image.getHeight() * 50 / 100;
-        image.scale(newWidth, newHeight);
-    }
-
-    public void act() 
-    {
-        movePlayer();
-    }    
-
-    private void movePlayer()
-    {
-        if(Greenfoot.isKeyDown("right")) 
-        {
-            setLocation(getX() + speed, getY());
-            setImage(imageRight);
-        }
-        if(Greenfoot.isKeyDown("left")) 
-        {
-            setLocation(getX() - speed, getY());
-            setImage(imageLeft);
-        }
-        if(Greenfoot.isKeyDown("down")) 
-        {
-            setLocation(getX(), getY() + speed);
-            setImage(imageDown);
-        }
-        if(Greenfoot.isKeyDown("up")) 
-        {
-            setLocation(getX(), getY() - speed);
-            setImage(imageUp);
-        }
+        Player player = new Player();
+        addObject(player,293,148);
     }
 }
