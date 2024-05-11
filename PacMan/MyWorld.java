c94fe75aa069f2955efef9a97b27274357f9e3e8
Iamatto01 <145031320+Iamatto01@users.noreply.github.com>
2024-05-11 19:45:08 +0800

    Update MyWorld.java

--- PacMan/MyWorld.java
+++ PacMan/MyWorld.java
@@ -1,35 +1,63 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class MyWorld extends World
+public class Player extends Actor
 {
+    private int speed = 3;  // Set the speed of the player
 
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
-    public MyWorld()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
-        GreenfootImage background = new GreenfootImage(getWidth(), getHeight());
-        background.setColor(Color.GREEN);
-        background.fill();
-        setBackground(background);
-        prepare();
-    }
-    /**
-     * Prepare the world for the start of the program.
-     * That is: create the initial objects and add them to the world.
-     */
-    private void prepare()
+    // Load the images for each direction
+    private GreenfootImage imageUp;
+    private GreenfootImage imageDown;
+    private GreenfootImage imageLeft;
+    private GreenfootImage imageRight;
+
+    public Player()
     {
-        Player player = new Player();
-        addObject(player,293,148);
+        imageUp = new GreenfootImage("image1x4.png");
+        imageDown = new GreenfootImage("image1x1.png");
+        imageLeft = new GreenfootImage("image1x2.png");
+        imageRight = new GreenfootImage("image1x3.png");
+
+        scaleImage(imageUp);
+        scaleImage(imageDown);
+        scaleImage(imageLeft);
+        scaleImage(imageRight);
+
+        setImage(imageUp);
+    }
+
+    private void scaleImage(GreenfootImage image)
+    {
+        int newWidth = image.getWidth() * 50 / 100;
+        int newHeight = image.getHeight() * 50 / 100;
+        image.scale(newWidth, newHeight);
+    }
+
+    public void act() 
+    {
+        movePlayer();
+    }    
+
+    private void movePlayer()
+    {
+        if(Greenfoot.isKeyDown("right")) 
+        {
+            setLocation(getX() + speed, getY());
+            setImage(imageRight);
+        }
+        if(Greenfoot.isKeyDown("left")) 
+        {
+            setLocation(getX() - speed, getY());
+            setImage(imageLeft);
+        }
+        if(Greenfoot.isKeyDown("down")) 
+        {
+            setLocation(getX(), getY() + speed);
+            setImage(imageDown);
+        }
+        if(Greenfoot.isKeyDown("up")) 
+        {
+            setLocation(getX(), getY() - speed);
+            setImage(imageUp);
+        }
     }
 }
